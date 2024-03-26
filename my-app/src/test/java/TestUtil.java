c756260e4b39593404d39f01cc109cbfe7241b7f
Bilgehan Tekin <bilgehan.2002@gmail.com>
2024-03-26 17:53:35 +0300

    Added new tests to the TestUtil.java file

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,9 +1,7 @@
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TestUtil {
   Util c;
@@ -13,4 +11,34 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+  public void testSizeOne() {
+    assertFalse(c.compute(1));
+  }
+
+  @Test
+  public void testEvenSize() {
+    assertFalse(c.compute(1, 2, 3, 4));
+  }
+
+  @Test
+  public void testArgumentZero() {
+    try {
+      c.compute(0, 1, 2);
+      fail();
+    } catch (RuntimeException e) {
+        assertTrue(true);
+    }
+  }
+
+  @Test
+  public void testNoDivisor() {
+    assertFalse(c.compute( 2, 2, 3));
+  }
+
+  @Test
+  public void testDivisor() {
+    assertTrue(c.compute( 1, 2, 3));
+  }
 }
