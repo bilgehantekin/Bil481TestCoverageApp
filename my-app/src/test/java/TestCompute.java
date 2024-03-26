db50e305871dee24a108c6cbcb23fa0fa18301ed
Bilgehan Tekin <bilgehan.2002@gmail.com>
2024-03-26 17:33:14 +0300

    Added new tests to the TestCompute.java file

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -14,4 +14,36 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void testZeroSize() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(0);
+    assertEquals(-1, c.countNumberOfOccurrences("a"));
+  }
+
+  @Test
+  public void testNoOccurrences() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("x")).thenReturn(false);
+    when(mq.getAt(0)).thenReturn("y");
+    when(mq.getAt(1)).thenReturn("y");
+    when(mq.getAt(2)).thenReturn("y");
+    assertEquals(0, c.countNumberOfOccurrences("x"));
+  }
+
+  @Test
+  public void testOccurrences() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains("x")).thenReturn(true);
+    when(mq.getAt(0)).thenReturn("x");
+    when(mq.getAt(1)).thenReturn("y");
+    when(mq.getAt(2)).thenReturn("x");
+    assertEquals(2, c.countNumberOfOccurrences("x"));
+  }
 }
